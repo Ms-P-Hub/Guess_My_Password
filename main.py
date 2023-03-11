b9f9d2d9d7ffcbeec4f2bdbef741314c16f2940e
Gugu Precious Mokwena <gugu.codes@gmail.com>
2023-03-11 19:54:08 +0200

    Initialized needed variables

---
+++ main.py
@@ -0,0 +1,8 @@
+import random
+
+# Initialise variables
+response1 = "I am afraid not. Please try again."
+response2 = "That is a good password but not my password. Keep going."
+response3 = "That is not my password. It's really easy to guess my password."
+response4 = "Well done! You must work for MI6. give my regards to James Bond."
+my_password = "my_password"
