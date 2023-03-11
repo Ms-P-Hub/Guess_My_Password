1dd5c314ec8dbb55fd2324e7849be2a3a80b4e3d
Gugu Precious Mokwena <gugu.codes@gmail.com>
2023-03-11 20:34:50 +0200

    Changes addressed

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import random
 
 # Initialise variables
-response1 = "I am afraid not. Please try again."
-response2 = "That is a good password but not my password. Keep going."
-response3 = "That is not my password. It's really easy to guess my password."
-response4 = "Well done! You must work for MI6. give my regards to James Bond."
+lose_response = ["I am afraid not. Please try again.","That is a good password but not my password. Keep going.","That is not my password. It's really easy to guess my password."]
+win_response = "Well done! You must work for MI6. Give my regards to James Bond."
 my_password = "my_password"
