8a7595f77119666d382a74b757b84d26bbe6e3f0
Gugu Precious Mokwena <gugu.codes@gmail.com>
2023-03-11 20:55:07 +0200

    Added a password Validation

--- main.py
+++ main.py
@@ -4,3 +4,9 @@ import random
 response = {"lose": ["I am afraid not. Please try again.","That is a good password but not my password. Keep going.","That is not my password. It's really easy to guess my password."],
 "win": "Well done! You must work for MI6. Give my regards to James Bond."}
 my_password = "my_password"
+
+def is_correct(guess,password):
+    if guess == password:
+        return True
+    else:
+        return False
